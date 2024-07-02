94a76fd207e72f8f2502d13da56f6969a80248fc
Inma Quesada Tirado <nihvix.art@gmail.com>
2024-07-02 20:32:10 +0200

    Eventos inline prueba

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/* ============================================================
+    JS para la comprobación de datos del formulario de entrada
+   ============================================================*/
+
+//Capturar el valor del input nick
+/*const nickInput=document.getElementById("nick");
+nickInput.value="Inma";
+console.log(nickInput.nodeType);
+console.log(nickInput.value);
+
+//Capturar el valor del input email
+const emailInput=document.getElementById("email");
+console.log(emailInput.value);
+
+//capturar el valor del select
+const selectInput=document.getElementById("size");
+console.log(selectInput.value);
+console.log(selectInput.options[selectInput.selectedIndex].text);*/
+
+/* ==============================
+            EVENTOS
+   ==============================*/
+function test(){
+    console.log("Event Inline test");
+}
