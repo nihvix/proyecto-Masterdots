9005b86406686954fb71c6f52418aa0e24900193
Inma Quesada Tirado <nihvix.art@gmail.com>
2024-07-02 20:47:33 +0200

    Prueba eventos Listener

--- js/app.js
+++ js/app.js
@@ -2,24 +2,24 @@
     JS para la comprobación de datos del formulario de entrada
    ============================================================*/
 
-//Capturar el valor del input nick
-/*const nickInput=document.getElementById("nick");
-nickInput.value="Inma";
-console.log(nickInput.nodeType);
-console.log(nickInput.value);
-
-//Capturar el valor del input email
-const emailInput=document.getElementById("email");
-console.log(emailInput.value);
-
-//capturar el valor del select
-const selectInput=document.getElementById("size");
-console.log(selectInput.value);
-console.log(selectInput.options[selectInput.selectedIndex].text);*/
+/* ==================================================
+            INICIALIZACIÓN VARIABLES, OBJETOS, DOM
+   ==================================================*/
+const playButton = document.getElementById("play");
 
 /* ==============================
             EVENTOS
    ==============================*/
-function test(){
-    console.log("Event Inline test");
+playButton.addEventListener('click', checkForm);
+playButton.addEventListener('click', executeAction);
+
+/* ==============================
+            FUNCIONES
+   ==============================*/
+function checkForm(){
+    console.log("We check the form");
+}
+function executeAction(){
+    console.log("We exe");
+    playButton.removeEventListener('click', checkForm);
 }
