6e88638bed23de6c9eec71913cd8cead9d74b215
Inma Quesada Tirado <nihvix.art@gmail.com>
2024-07-03 22:33:01 +0200

    Novedades js + Css arreglado centrado

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@
 const nickInput = document.getElementById("nick");
 const sizeInput = document.getElementById("size");
 const formInput = document.getElementById("inputForm");
+const error=document.getElementById("error");
 
 /* ==============================
             EVENTOS
@@ -17,14 +18,17 @@ formInput.addEventListener('submit', checkForm);
 /* ==============================
             FUNCIONES
    ==============================*/
-function checkForm(){
+function checkForm(event){
     if(nickInput.value.length==0){
         console.log("There's no nick");
-        nickInput.focus(); //El cursor se coloca sobre el input
+        event.preventDefault(); //El cursor se coloca sobre el input
+        error.innerText="Nick field cannot be empty!";
         return false;
     }else if(sizeInput.value="0"){
         console.log("No size selected yet!");
         sizeInput.focus();
+        event.preventDefault();
+        error.innerText="A size must be selected!";
         return false;
     }
     return true;
