66b57ebb7d8575f82b756fb0cbef2137d1cd1c18
Inma Quesada Tirado <nihvix.art@gmail.com>
2024-07-04 00:32:00 +0200

    Regex + CSS modificado

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 const nickInput = document.getElementById("nick");
 const sizeInput = document.getElementById("size");
 const formInput = document.getElementById("inputForm");
-const error=document.getElementById("error");
+const error = document.getElementById("error");
 
 /* ==============================
             EVENTOS
@@ -18,17 +18,19 @@ formInput.addEventListener('submit', checkForm);
 /* ==============================
             FUNCIONES
    ==============================*/
-function checkForm(event){
-    if(nickInput.value.length==0){
-        console.log("There's no nick");
-        event.preventDefault(); //El cursor se coloca sobre el input
-        error.innerText="Nick field cannot be empty!";
-        return false;
-    }else if(sizeInput.value="0"){
-        console.log("No size selected yet!");
-        sizeInput.focus();
+function checkForm(event) {
+    if (nickInput.value.length == 0) {
         event.preventDefault();
-        error.innerText="A size must be selected!";
+        error.innerText = "Nick field cannot be empty";
+        return false;
+    } else if (nickInput.value.match(/\b\d+\w*/)) { /*Para indicar inicio/fin del regex se usa / no comillas*/
+        event.preventDefault();
+        error.innerText = "Name field cannot start by a number";
+        return false;
+    } else if (sizeInput.value == "0") {
+        sizeInput.focus();//El cursor se coloca sobre el input
+        event.preventDefault();
+        error.innerText = "A size must be selected";
         return false;
     }
     return true;
