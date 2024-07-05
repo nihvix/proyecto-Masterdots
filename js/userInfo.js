6e47d26d0cba8434fba979c2f695c8b437764838
Inma Quesada Tirado <nihvix.art@gmail.com>
2024-07-05 12:28:07 +0200

    Historial

--- js/userInfo.js
+++ js/userInfo.js
@@ -6,22 +6,40 @@
 var nick;
 
 //Nick será un objeto con la información del input nick
-function userData(nickInput){
+function userData(nickInput) {
 
     sessionStorage.setItem("nick", nickInput.value); //"nick" será la clave usada para recuperar la info
 }
 
-function getUserData(){
-    nick=sessionStorage.getItem("nick");
+function getUserData() {
+    nick = sessionStorage.getItem("nick");
     console.log(nick);
 }
 
 //Función para comprobar que nos meten los datos en el sessionStorage
 //así no se lo pueden saltar poniendo "game.html"
-function checkUserData(){
-    if(nick==null){ //Es decir, no hay SessionStorage
+function checkUserData() {
+    if (nick == null) { //Es decir, no hay SessionStorage
         sessionStorage.setItem("error", "Form not filled correctly!");
         return false;
     }
     return true;
+}
+
+//localStorage -- registro fecha de entrada
+function userDateRecord(nick) {
+    let recordStorage = localStorage.getItem("record"); //obtenemos el historial de entradas
+    let records;
+    if (recordStorage == null) { //no existe un historial previo
+        records = []; //lo creamos
+    }else{
+        records=JSON.parse(recordStorage); //Si existe, lo obtenemos del localStorage haciendo
+        //la operación inversa al JSON.strongify, ya que en el local se ha almacenado en JSON 
+    }
+    let userRecord = {
+        user: nick.value,
+        date: Date.now() //Date: objeto que representa fechas en JS
+    }
+    records.push(userRecord);
+    localStorage.setItem("record", JSON.stringify(userRecord));
 }
