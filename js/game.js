241cb260b2c94e309a61d8df250162e936c234ce
Inma Quesada Tirado <nihvix.art@gmail.com>
2024-07-13 23:29:47 +0200

    Draw panel w colors (not working yet)

--- js/game.js
+++ js/game.js
@@ -5,21 +5,42 @@
 /* ==========================
             FUNCIONES
    ==========================*/
+/**
+ * Función que devuelve un número random entre 0 y max
+ * @param {int} max 
+ * @returns 
+ */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+/**
+ * Función para obtener la información del usuario de la parte derecha del menú
+ */
 function fillUserForm() {
     document.getElementById("nick").value = nick;
     document.getElementById("avatarImg").src = avatarImg; //variable de userInfo que almacena el avatar
 }
 
+/**
+ * Función que dibuja el panel de fichas
+ */
 function drawPanel() {
     document.getElementById("game").style.gridTemplateColumns = "repeat(" + size + ", 1fr)";
     document.getElementById("game").style.gridTemplateRows = "repeat(" + size + ", 1fr)";
     //Elementos de forma automática
-    let sizeInt=parseInt(size); //Hay que recordar que el valor de size en el sessionStorage se almacena como string
-    let items="";
+    let sizeInt = parseInt(size); //Hay que recordar que el valor de size en el sessionStorage se almacena como string
+    let items = "";
+    let color = ["red", "green"];
+    let colorRandom = 0;
     for (let index = 0; index < sizeInt * sizeInt; index++) {
-        items+=`<div class="containerItem"><div class="item"></div></div>`; //Comillas evaluativas
+        if (index % 2 > 0) {
+            colorRandom = getRandomInt(2);
+        }
+        items += `<div class="containerItem"><div class="item ${color[colorRandom]}"></div></div>`; //Comillas evaluativas para poder meter variable
     }
-}   document.getElementById("game").innerHTML=items;
+    document.getElementById("game").innerHTML = items;
+}
 
 /* =======================================
             LLAMADAS A FUNCIONES
